b5922da9cc957fc1bfb18479ac612b515d3b35a1
Mohsen Mohkami <mohkami.mohsen@gmail.com>
2016-08-21 17:13:15 -0600

    addded solution for 11172

---
+++ src/p11172/Main.java
@@ -0,0 +1,136 @@
+package p11172;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Created by Mason on 2016-08-16.
+ */
+public class Main
+{
+    private Utils inUtils;
+
+    private void run(boolean runningLocally)
+    {
+
+        String resLines = "";
+        int testCases = inUtils.readOneIntLine();
+        for (int i = 0; i < testCases; i++)
+        {
+            int[] aAndB = inUtils.getNextNDigitFromLine(2);
+            int a = aAndB[0];
+            int b = aAndB[1];
+
+            if (a == b)
+            {
+                resLines += '=';
+            } else
+            {
+                resLines += (a < b) ? '<' : '>';
+            }
+            resLines += '\n';
+        }
+
+        if (runningLocally)
+        {
+            Utils outUtils = new Utils("expected_output.txt");
+            String line;
+            String lines = "";
+            while ((line = outUtils.readLine()) != null)
+            {
+                lines += line + "\n";
+            }
+
+//        outUtils.printLine(lines1 + "\n --------- ");
+//        outUtils.printLine(lines + "\n --------- ");
+            outUtils.printLine(lines.equals(resLines) + "");
+        } else
+        {
+            System.out.print(resLines);
+        }
+    }
+
+    private Main(String inputFile)
+    {
+        inUtils = new Utils(inputFile);
+    }
+
+    public static void main(String[] arg)
+    {
+        String inputFile = "";
+        if (arg.length > 0)
+        {
+            inputFile = arg[0];
+        }
+        Main m = new Main(inputFile);
+        m.run(arg.length > 0);
+    }
+
+    private class Utils
+    {
+        Scanner scanner;
+
+        Utils(String inputFile)
+        {
+            if (!inputFile.isEmpty())
+            {
+                File file = new File(getCurrentDirectory() + inputFile);
+
+                try
+                {
+                    scanner = new Scanner(file);
+                } catch (FileNotFoundException e)
+                {
+                    e.printStackTrace();
+                    System.out.print("File not found");
+                }
+            } else
+            {
+                scanner = new Scanner(System.in);
+            }
+        }
+
+        private String getCurrentDirectory()
+        {
+            return System.getProperty("user.dir") + "\\src\\";
+        }
+
+        int readOneIntLine()
+        {
+            return getNextNDigitFromLine(1)[0];
+        }
+
+        int[] getNextNDigitFromLine(int n)
+        {
+            return getNDigitFromLine(inUtils.readLine(), n);
+        }
+
+        int[] getNDigitFromLine(String line, int n)
+        {
+            String[] words = line.split("\\s+");
+            int[] values = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                values[i] = Integer.valueOf(words[i]);
+            }
+            return values;
+        }
+
+        String readLine()
+        {
+            String line = null;
+            if (scanner.hasNext())
+            {
+                line = scanner.nextLine();
+            }
+            return line;
+        }
+
+        void printLine(String line)
+        {
+            System.out.println(line);
+        }
+    }
+}
